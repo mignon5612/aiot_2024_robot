891e62767a1d2488a7e89fb5cef1612bb56f5c30
mignon5612 <mignon5612@naver.com>
2024-09-19 14:23:31 +0900

    hi

---
+++ python/a15_booleen.py
@@ -0,0 +1,13 @@
+def main():
+    bo = bool()
+    bo1 = True
+    bo1 = [] # no element is False
+    bo1 = 0.0000001
+    if bo1:
+        print("bo1 is True")
+    else:
+        print("bo1 is False")
+
+
+if __name__=="__main__":
+    main()
---
+++ python/a16_find_method.py
@@ -0,0 +1,5 @@
+def main():
+    output_a = "안녕안녕하세요.".find()
+
+if __name__=="__main__":
+    main()
