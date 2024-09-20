889a42fd78d95814dad606397db6594cf6595efb
mignon5612 <mignon5612@naver.com>
2024-09-20 10:01:50 +0900

    yap

--- python/a15_booleen.py
+++ python/a15_booleen.py
@@ -3,10 +3,18 @@ def main():
     bo1 = True
     bo1 = [] # no element is False
     bo1 = 0.0000001
+    bo1 = ""
     if bo1:
         print("bo1 is True")
     else:
         print("bo1 is False")
+
+    #c였다면 (!true) -> false
+    #python (not True) -> False
+    #c ( ACondition && BCondition)
+    #python (ACondition and BCondition)
+    #c ( ACondition || BCondition)
+    #python (ACondition or BCondition) 훨씬 직관적이고 읽기쉽다
 
 
 if __name__=="__main__":
