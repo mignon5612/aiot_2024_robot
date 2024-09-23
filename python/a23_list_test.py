39b631dea2a75c1bf08ff9786646be4317b8f310
mignon5612 <mignon5612@naver.com>
2024-09-23 12:26:26 +0900

    hi

--- python/a23_list_test.py
+++ python/a23_list_test.py
@@ -10,5 +10,15 @@ def main():
     li1[1] = "삼삼삼"
     print(li1)
 
+    # 리스트 연산자
+    list_a = [1,2,3]
+    list_b = [4,5,6]
+
+    print(list_a + list_b)
+    # list_a.extend(list_b)
+    print(list_a)
+    print(list_a * 3)
+    print(len(list_a))
+
 if __name__=="__main__":
     main()
